091a3c9abb197d0643932da433c6e878132ee795
Godefroy <courtin.godefroy@gmail.com>
2020-10-23 11:07:12 +0200

    set up config et templates

--- mytravelbook/travels/views.py
+++ mytravelbook/travels/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def acceuil(request):
+    context = {}
+    return render(request, "travels/acceuil.html", context)
+
+
+def travel(request):
+    context = {}
+    return render(request, "travels/travel.html", context)
