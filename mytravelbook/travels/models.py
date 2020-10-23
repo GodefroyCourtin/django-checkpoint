8faa5f647a772662e851eee88d7c5053fb01b6ad
Godefroy <courtin.godefroy@gmail.com>
2020-10-23 13:04:08 +0200

    fin du checkpoint

--- mytravelbook/travels/models.py
+++ mytravelbook/travels/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Voyage(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    #start_date 
+    #end_date
+    def __str__(self):
+        return self.title
+
+class Voyagestep(models.Model):
+    voyage = models.ForeignKey(Voyage, on_delete=models.CASCADE) 
+    city = models.CharField(max_length=200)
+    #date
+    resume = models.CharField(max_length=800)
+
+    def __str__(self):
+        return self.name
+
+class Linkstep(models.Model):
+    voyagestep = models.ForeignKey(Voyagestep, on_delete=models.CASCADE) 
+    linkrestaurant = models.URLField()
+    linkphoto = models.URLField()
+    
+    
