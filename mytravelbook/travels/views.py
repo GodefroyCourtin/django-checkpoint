9b27733e05893286f94d284720a4480686ca2b69
Godefroy <courtin.godefroy@gmail.com>
2020-10-25 04:24:00 +0100

    padding h6 ok

--- mytravelbook/travels/views.py
+++ mytravelbook/travels/views.py
@@ -4,8 +4,10 @@ from .models import Voyage, Step, Linkstep
 # Create your views here.
 def acceuil(request):
     # voyages = Voyage.objects.all()
-    voyages = Voyage.objects.prefetch_related("step_set").prefetch_related(
-        "step_set__linkstep_set"
+    voyages = (
+        Voyage.objects.prefetch_related("step_set")
+        .prefetch_related("step_set__linkstep_set")
+        .order_by("-end_date")
     )
 
     # voyages = Linkstep.objects.select_related("step").all()
